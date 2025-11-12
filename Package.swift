4bd3a53c67e3353af37a75df1c3ca7bfb063cd78
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-12 21:50:02 +0100

    chore: update RFC 3987 dependency to 0.2.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-rfc-3987.git", from: "0.1.0")
+        .package(url: "https://github.com/swift-standards/swift-rfc-3987.git", from: "0.2.0")
     ],
     targets: [
         .target(
